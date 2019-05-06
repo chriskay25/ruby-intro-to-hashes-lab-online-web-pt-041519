40042877a60e685ee4a84518285987a85ea2eef4
Chris Kay <chriskay25@gmail.com>
2019-05-06 21:52:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -54,7 +54,7 @@ monopoly =
 	      {
 	        :reading_railroad => 
 	          {
-	            "mortgage value" => "$100"
+	            "mortgage_value" => "$100"
 	          },
 	        :pennsylvania_railroad => 
 	          {
